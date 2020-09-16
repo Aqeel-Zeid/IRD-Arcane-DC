ce4c021f40fb64fea45d3bff632c6e695dec0387
Mohamed Rifan <mohamedrifan@live.com>
2020-09-16 16:19:34 +0530

    Directory Structure Initialization Functions Complete

--- ird_arcanedc_desktop/src/Api/Controllers/testRouts.js
+++ ird_arcanedc_desktop/src/Api/Controllers/testRouts.js
@@ -1,11 +1,13 @@
+const FileAndFolderHelper = require("../helpers/FileAndFolderHelper.js")
 
 const express = require('express')
 , router = express.Router()
 
 
   router.get('/testRoute1', (req, res) => {
-    console.log(req.app.locals.folderPath)
-    res.send('Test Route').status(200);
+    //console.log(req.app.locals.folderPath)
+    FileAndFolderHelper.initializeAppFolderStructure(req.app.locals.folderPath);
+    res.send('Test Route Folder Path = ' + req.app.locals.folderPath ).status(200);
   })
 
 
--- ird_arcanedc_desktop/src/Api/helpers/FileAndFolderHelper.js
+++ ird_arcanedc_desktop/src/Api/helpers/FileAndFolderHelper.js
@@ -1,24 +1,35 @@
 const fs = require('fs');
-
-
-
+ 
 //Initialize App Data Folder with Structure 
-function initializeAppFolderStructure() {
+function initializeAppFolderStructure(baseFolderName) {
     
-    if(isInitialized() !== true )
-    {
+    const appFolderName = baseFolderName + `/ArcaneDC`;
 
+    if(isInitialized(appFolderName) !== true )
+    {
+        //console.log("Directory Doesnt Exist", appFolderName)
+        fs.mkdirSync(appFolderName)
+        fs.mkdirSync(appFolderName + '/Preferences')
+        fs.mkdirSync(appFolderName + '/Users')
+        fs.mkdirSync(appFolderName + '/Projects')
+        fs.mkdirSync(appFolderName + '/ReasearchData')
+        fs.mkdirSync(appFolderName + '/AppData')  
     }
     else
     {
-
+        //console.log("Directory Exist")
     }
 
 }
 
 //Check Wether the Application Folder Structure exist in the OS dependent APP data Directory
-function isInitialized(){
-
+function isInitialized(dir){
+    
+    if (fs.existsSync(dir)) {
+        return true;
+    } else {
+        return false;
+    }
 
 }
 
