d1389457623222394cfecc3bebb187bfc71f8ebf
Aqeel <aqeelaqeelzeid@gmail.com>
2020-09-16 16:33:26 +0530

    More Changes SideBar

--- ird_arcanedc_desktop/src/Components/Layouts/SideBar.jsx
+++ ird_arcanedc_desktop/src/Components/Layouts/SideBar.jsx
@@ -64,6 +64,7 @@ export default function PermanentDrawerLeft() {
         <Divider />
         <List>
           {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            
             <ListItem button key={text}>
               <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
               <ListItemText primary={text} />
